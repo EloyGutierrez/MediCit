2b83b75e6a3e66dab4024f5ee6caa059705e40dc
MAYRA ALEJANDRA BRAVO ALARCON <A16200010@idat.edu.pe>
2020-07-06 15:35:39 -0500

    Agregando imagen e información del hospital Hipólito Unanue

---
+++ app/src/main/java/moviles/aplicaciones/medicit/InformacionHospital.java
@@ -0,0 +1,14 @@
+package moviles.aplicaciones.medicit;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InformacionHospital extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informacionhospital);
+    }
+
+}
