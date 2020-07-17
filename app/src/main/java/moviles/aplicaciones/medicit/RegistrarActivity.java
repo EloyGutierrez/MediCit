ea1f49b9cd3bea5976805a3c1784ce8a59319c90
EloyGutierrez <EloyGutierrez@users.noreply.github.com>
2020-07-17 17:27:53 -0500

    array en combodias y combomes en RegistrarActivity

--- app/src/main/java/moviles/aplicaciones/medicit/RegistrarActivity.java
+++ app/src/main/java/moviles/aplicaciones/medicit/RegistrarActivity.java
@@ -7,16 +7,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class RegistrarActivity extends AppCompatActivity {
-    Spinner combosexo;
+    Spinner combosexo,combomes,combodia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
         combosexo = findViewById(R.id.idspinnerSexo);
-
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.combo_sexo,R.layout.spinner_item_estilos);
-        combosexo.setAdapter(adapter);
-
+        combomes = findViewById(R.id.idspinnermes);
+        combodia = findViewById(R.id.idspinnerdia);
+        //array para el spinner de sexo
+        ArrayAdapter<CharSequence> sexoadapter= ArrayAdapter.createFromResource(this,R.array.combo_sexo,R.layout.spinner_item_estilos);
+        combosexo.setAdapter(sexoadapter);
+        //array para el spinner de mes
+        ArrayAdapter<CharSequence> mesadapter= ArrayAdapter.createFromResource(this,R.array.combo_mes,R.layout.spinner_item_estilos);
+        combomes.setAdapter(mesadapter);
+        //array para el spinner de dia
+        ArrayAdapter<CharSequence> diaadapter= ArrayAdapter.createFromResource(this,R.array.combo_dia,R.layout.spinner_item_estilos);
+        combodia.setAdapter(diaadapter);
 
     }
 }
