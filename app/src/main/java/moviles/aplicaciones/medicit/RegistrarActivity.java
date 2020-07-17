3514e885b40ef9f7cd0a416a47ec5f8b162d8472
EloyGutierrez <EloyGutierrez@users.noreply.github.com>
2020-07-17 18:40:45 -0500

    diseño de la activity_registrar.xml ¡hecho!

--- app/src/main/java/moviles/aplicaciones/medicit/RegistrarActivity.java
+++ app/src/main/java/moviles/aplicaciones/medicit/RegistrarActivity.java
@@ -2,12 +2,14 @@ package moviles.aplicaciones.medicit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class RegistrarActivity extends AppCompatActivity {
-    Spinner combosexo,combomes,combodia;
+    Spinner combosexo,combomes,combodia,comboseguro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +17,7 @@ public class RegistrarActivity extends AppCompatActivity {
         combosexo = findViewById(R.id.idspinnerSexo);
         combomes = findViewById(R.id.idspinnermes);
         combodia = findViewById(R.id.idspinnerdia);
+        comboseguro = findViewById(R.id.idspinnerseguro);
         //array para el spinner de sexo
         ArrayAdapter<CharSequence> sexoadapter= ArrayAdapter.createFromResource(this,R.array.combo_sexo,R.layout.spinner_item_estilos);
         combosexo.setAdapter(sexoadapter);
@@ -24,6 +27,13 @@ public class RegistrarActivity extends AppCompatActivity {
         //array para el spinner de dia
         ArrayAdapter<CharSequence> diaadapter= ArrayAdapter.createFromResource(this,R.array.combo_dia,R.layout.spinner_item_estilos);
         combodia.setAdapter(diaadapter);
+        //array para el spinner de seguro
+        ArrayAdapter<CharSequence> seguroadapter= ArrayAdapter.createFromResource(this,R.array.combo_seguro,R.layout.spinner_item_estilos);
+        comboseguro.setAdapter(seguroadapter);
 
+    }
+    public void Principal(View view){
+        Intent i =new Intent(this, PrincipalActivity.class);
+        startActivity(i);
     }
 }
