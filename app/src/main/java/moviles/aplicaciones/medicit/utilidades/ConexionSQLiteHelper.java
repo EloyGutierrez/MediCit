88bd6df9a03294fa8f16c1762a44b81a5dae5ca5
EloyGutierrez <EloyGutierrez@users.noreply.github.com>
2020-07-24 11:07:46 -0500

    creando la clase Utilidades.java

--- app/src/main/java/moviles/aplicaciones/medicit/utilidades/ConexionSQLiteHelper.java
+++ app/src/main/java/moviles/aplicaciones/medicit/utilidades/ConexionSQLiteHelper.java
@@ -1,5 +1,26 @@
 package moviles.aplicaciones.medicit.utilidades;
 
-public class ConexionSQLiteHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+    final String CREAR_TABLA_USUARIO = "CREATE TABLE usuarios(dni INTEGER, nombre TEXT, apellidopaterno TEXT, apellidomaterno TEXT, sexo TEXT, fechadenacimiento DATE, correo TEXT, celular INTEGER, seguro TEXT, contrasenia TEXT)";
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREAR_TABLA_USUARIO);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+        onCreate(db);
+    }
 }
