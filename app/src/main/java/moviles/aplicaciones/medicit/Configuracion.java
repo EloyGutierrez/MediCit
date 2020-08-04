71b58f77a8ad30e35d428fada389192de99874cf
Shimi2000 <56854873+Shimi2000@users.noreply.github.com>
2020-08-04 10:16:39 -0500

    Haciendo cambios en activity_configuracion.xml

--- app/src/main/java/moviles/aplicaciones/medicit/Configuracion.java
+++ app/src/main/java/moviles/aplicaciones/medicit/Configuracion.java
@@ -1,4 +1,24 @@
 package moviles.aplicaciones.medicit;
 
-public class Configuracion {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Configuracion extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LayoutInflater i = getLayoutInflater();
+        View view= i.inflate(R.layout.activity_configuracion,container,false);
+
+        return view;
+    }
+
+
+
 }
