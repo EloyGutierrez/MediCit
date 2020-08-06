529abd65b662ca9622031c4d865736a47ff58971
Shimi2000 <56854873+Shimi2000@users.noreply.github.com>
2020-08-06 15:53:32 -0500

    Agregando librerias para seleccionar imagen.

--- app/src/main/java/moviles/aplicaciones/medicit/cerrarsesion.java
+++ app/src/main/java/moviles/aplicaciones/medicit/cerrarsesion.java
@@ -1,6 +1,14 @@
 package moviles.aplicaciones.medicit;
 
+import android.view.Menu;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class cerrarsesion extends Fragment {
+    @Override
+    public void onOptionsMenuClosed(@NonNull Menu menu) {
+        super.onOptionsMenuClosed(menu);
+
+    }
 }
